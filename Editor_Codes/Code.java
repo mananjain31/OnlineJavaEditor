566314b3bcba292b5c603c13ce5bd0d35d394bc3
Manan Jain <mananjain31jan@gmail.com>
2021-08-23 15:42:26 +0530

    renamed folder

--- Editor_Codes/Code.java
+++ Editor_Codes/Code.java
@@ -1,12 +1,12 @@
-class FirstProgram 
+class MyCode
 {
-	public static void main(String...args)
-	{
-		System.out.print("This is my first Java Program");
-	}
+public static void main(String...args)
+{
+System.out.println("Hey There Coder");
+}
 }
 
 class inputTaker{public static void main(String...args) throws Exception{
 try{System.setIn(new java.io.FileInputStream("inputFile.txt"));}catch(Exception e){}
-FirstProgram.main(null);
+MyCode.main(null);
 }}
